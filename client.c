5412c2cd40bda49fb037cfdaee7a1df20332994a
Hsinwei Lin <104602727+Hsinweilin@users.noreply.github.com>
2024-01-30 13:43:33 -0700

    setup client, server sockets connection

--- client.c
+++ client.c
@@ -12,19 +12,32 @@
 int main (int argc, char * argv[])
 {
     // client socket
-    int s = socket(PF_INET, int SOCK_STREAM, 0);
+    int s = socket(PF_INET, SOCK_STREAM, 0);
     // server address struct
     struct sockaddr_in serAddr;
     // clear serAddr
     bzero(&serAddr, sizeof(serAddr));
     // assign values of the server address struct
     serAddr.sin_family = AF_INET;
-    serAddr.sin_addr.s_addr = inet_addr(argv[0]);
-    serAddr.sin_port = hton(argv[1]);
+    serAddr.sin_addr.s_addr = inet_addr(argv[1]);
+    serAddr.sin_port = htons(atoi(argv[2]));
 
     // connect
-    connect(s, (stuct socketaddr *) &serAddr, sizeof(serAddr));
-
+    int connection_status = connect(s, (struct sockaddr *) &serAddr, sizeof(serAddr));
+    perror("connection failed\n");
+    printf("connection_status:%d\n", connection_status);
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t size;
+    if (connection_status == 0) {// if connect success
+        while ((size = getline(&line, &len, stdin)) != -1) {
+            int send_status = send(s, line, sizeof(line), 0);
+            printf("%zd %s\n", size, line);
+        }
+    }
+    else{
+        perror("connection failed\n");
+    }
 }
 
 
--- server.c
+++ server.c
@@ -6,37 +6,41 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <string.h>
+#include <unistd.h>  // Include for close function
 
-int main() {
+int main(int argc, char * argv[]) {
     // server socket
-    int s = socket(PF_INET, int SOCK_STREAM, 0);
+    int s = socket(PF_INET, SOCK_STREAM, 0);
     // server address struct
     struct sockaddr_in myAddr;
     bzero(&myAddr, sizeof(myAddr));
     // assign values of server address struct
     myAddr.sin_family = AF_INET;
     myAddr.sin_addr.s_addr = INADDR_ANY;
-    myAddr.sin_port = hton(80);
+    myAddr.sin_port = htons(atoi(argv[1]));
 
     // bind server address struct to server socket
-    bind(s, (struct sockaddr *) &myAddr, sizeof(myAddr));
+    int bind_status = bind(s, (struct sockaddr *) &myAddr, sizeof(myAddr));
+    printf("bind_status%d\n", bind_status);
     // keep at most 5 connections
     listen(s, 5);
 
     // client address/port
     struct sockaddr_in cliAddr;
     bzero(&cliAddr, sizeof(cliAddr));
-    len = sizeof(cliAddr);
+    socklen_t len = sizeof(cliAddr);
 //    // new socket for new connections
 //    ns = accept(s, (struct sockaddr *) &cliAddr, &len);
 
+    char buf[1024];
     // constantly receive client address/port
-    buf = char[1024];
     while (1) {
         // new socket for new connections
-        ns = accept(s, &cliAddr, &len);
+        int ns = accept(s, (struct sockaddr *) &cliAddr, &len);
+        ssize_t n = 0;
         while (n = recv(ns, buf, sizeof(buf), 0)) {
-            printf("%s\n", n);
+            printf("%zd %s\n", n, buf);
         }
         close(ns);
     }
