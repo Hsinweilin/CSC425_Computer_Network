228211afb4580e255585a9291c6c233aa9228957
Hsinwei Lin <104602727+Hsinweilin@users.noreply.github.com>
2024-03-24 16:43:12 -0700

    Finish adding header

--- client.c
+++ client.c
@@ -9,13 +9,48 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 
-void callSelect(int s, int ns) {//select() code
+struct Header{
+    int messageType;// 1 = actual data, 0 = heartbeat message
+    int ack; // acknowledgment number
+    int seq; // sequence number
+    int sessionID;// keep track of session
+};
+void setAndSendHeader(int socket, int seq, int ack, int sID, int msg_type){
+    struct Header header;
+    header.sessionID = sID;
+    header.messageType = msg_type;
+    header.seq = seq;
+    header.ack = ack;
+    char headerBuf[sizeof(struct Header)];
+    memcpy(headerBuf, &header, sizeof(struct Header));
+    send(socket, headerBuf, sizeof(struct Header), 0);//send the first heartbeat message to sproxy
+}
+void sendHeartBeat(int s, int sID){
+    struct Header header;
+    header.sessionID = sID;
+    header.messageType = 0;
+    header.ack = -1;
+    header.seq = -1;
+    char headerBuf[sizeof(struct Header)];
+    memcpy(headerBuf, &header, sizeof(struct Header));
+    int res = send(s, headerBuf, sizeof(struct Header), 0);//send the first heartbeat message to sproxy
+    printf("send heatbeat status: %d\n", res);
+    uint32_t networkPayload = htonl((uint32_t)sID);
+    int res2 = send(s, &networkPayload, sizeof(uint32_t), 0);// send the heartbeat playload to sproxy
+    printf("send playload status: %d\n", res2);
+}
+void callSelect(int s, int ns, int sID) {//select() code
+    int sessionID = sID;
+    time_t lastReceiveTime, lastTransmitTime;
+    //int seq_C, ack_C, seq_S, ack_S = -1;
     while(1){
         int n, MAX_LEN = 256;
         struct timeval tv;
         fd_set readfds;
-        char buf1[MAX_LEN], buf2[MAX_LEN];
+//        char buf1[MAX_LEN], buf2[MAX_LEN];
         //clear the set
         FD_ZERO(&readfds);
         //add descriptors to the set
@@ -28,8 +63,9 @@ void callSelect(int s, int ns) {//select() code
             n = ns + 1;
 
         //wait until either socket has data ready to recv() timeout 10.5 secs
-        tv.tv_sec = 10;
-        tv.tv_usec = 500000;
+        //time_t time1 = time(NULL);//get currecnt time
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
         //select will return when at least one of the sockets has incoming traffic, or after timeout
         int rv = select(n, &readfds, NULL, NULL, &tv);
 
@@ -37,27 +73,60 @@ void callSelect(int s, int ns) {//select() code
             perror("select");
             return;
         }
-        else if (rv == 0) {
-            printf("Timeout occurred! No data after 10.5 seconds\n");
+        else if (rv == 0) {//Timeout occurred! No data after 1 seconds, means no data send out as well
+            //printf("Timeout occurred! No data after 1 seconds\n");
+//            struct Header heartBeat;
+//            heartBeat.messageType = 0//means heartbeat message
+//            send(s, buf2, fromTelnet_len, 0);//send heartbeat data to sproxy
+            time_t newSendTime, newReceiveTime;
+            newSendTime = time(NULL);
+            newReceiveTime = time(NULL);
+            if ((newSendTime - lastTransmitTime) >= 1){
+                //todo send a heart beat message
+                sendHeartBeat(s, sessionID);
+            }
+            else if((newReceiveTime - lastReceiveTime) >= 3){
+                //todo timeout need to reconnect
+                return;//since this connect is already lost
+            }
         }
         else{
             // one or both of the descriptors have data
-            if (FD_ISSET(s, &readfds)) {
+
+            if (FD_ISSET(s, &readfds)) {//receive from sproxy
+                char buf1[MAX_LEN];
+                char headerBuf[sizeof(struct Header)];
+                if (recv(s, headerBuf, sizeof(struct Header), 0) < 0){//if receives fail
+                    perror("error receiving header");
+                }
+                struct Header *receivedHeader = (struct Header *)headerBuf;//make the header buffer into Header struct
+                printf("Received Header - messageType: %d, Seq: %d, Ack: %d, sessionID: %d\n", receivedHeader->messageType, receivedHeader->seq, receivedHeader->ack, receivedHeader->sessionID);
                 ssize_t fromSproxy_len = recv(s, buf1, MAX_LEN, 0);//receive data from sproxy
-                if (fromSproxy_len == 0)
-                    return;
-                //buf1[fromSproxy_len] = '\0';
-                //printf("%s\n", buf1);//print received data
-                send(ns, buf1, fromSproxy_len, 0);//send data to telnet
+                if (fromSproxy_len > 0){//if receive bytees
+                    if (receivedHeader->messageType == 0) {//it's a heartbeat message, just receive it, do nothing
+                        //TODO: update timestamp, and check session ID
+                    }
+                    else{//it's a actual data packet, need to receive and forward
+                        send(ns, buf1, fromSproxy_len, 0);//send data to telnet
+                    }
+                }
+                else if (fromSproxy_len == 0)
+                    return;//connection closed, return
+                else
+                    return;//error happened, return
             }
 
-            if (FD_ISSET(ns, &readfds)) {
-                ssize_t fromTelnet_len = recv(ns, buf2, MAX_LEN, 0);//receive data from telnet
-                if (fromTelnet_len == 0)
-                    return;
-                //buf2[fromTelnet_len] = '\0';
-                //printf("%s\n", buf2);//print received data
-                send(s, buf2, fromTelnet_len, 0);//send data to sproxy
+            if (FD_ISSET(ns, &readfds)) {//receive data from telnet
+                char buf2[MAX_LEN];
+                ssize_t fromTelnet_len = recv(ns, buf2, MAX_LEN, 0);
+                if (fromTelnet_len > 0){
+                    setAndSendHeader(s, 0, 0, sessionID, 1);//set and send header
+                    send(s, buf2, fromTelnet_len, 0);//send playload to sproxy
+                }
+                else if (fromTelnet_len == 0)
+                    return;//connection closed, return
+                else
+                    return;//error happened, return
             }
         }
     }
@@ -123,9 +192,10 @@ int main (int argc, char * argv[])
     bzero(&cliAddr, sizeof(cliAddr));
     socklen_t tlen = sizeof(cliAddr);
     while(1) {
+        int sessionID;//keep track of this session ID
         int ns = accept(t, (struct sockaddr *) &cliAddr, &tlen);
         if (ns > 0) {// if connection from telnet success, try to connect to sproxy
-            //printf("connect to telnet success\n");
+            printf("connect to telnet success\n");
             //create connection to sproxy
             int s = socket(PF_INET, SOCK_STREAM, 0);//socket to sproxy
             struct sockaddr_in serAddr;
@@ -138,15 +208,22 @@ int main (int argc, char * argv[])
 
             // connect cproxy socket to sproxy socket
             int connection_status = connect(s, (struct sockaddr *) &serAddr, sizeof(serAddr));
-            char *line = NULL; // a null line pointer that will be pointing to the line that is being read into the buffer
-            size_t len = 0; // capacity of the buffer, initialized as 0, let getline know the initial size of buffer, and can be updated by getline to have the actual size of buffer
-            ssize_t size; // actual length of input line in bytes
+            //char *line = NULL; // a null line pointer that will be pointing to the line that is being read into the buffer
+            //size_t len = 0; // capacity of the buffer, initialized as 0, let getline know the initial size of buffer, and can be updated by getline to have the actual size of buffer
+            //ssize_t size; // actual length of input line in bytes
             if (connection_status == 0) {// if connect success
-                //printf("Connect to sproxy success\n");
-                callSelect(s, ns);//call the select method
+                printf("connect to sproxy success\n");
+                // Seed the random number generator
+                srand(time(NULL));
+                // Generate a random number within the range of 0 to 100
+                int randomNum = rand() % 101;  // rand() generates a number between 0 and RAND_MAX, so we use % 101 to get a number between 0 and 100
+                sessionID = randomNum;
+                sendHeartBeat(s, randomNum);//send first heartBeat message to sproxy with random generated session ID
+                printf("sent out first heartBeat to sproxy\n");
+                callSelect(s, ns, sessionID);//call the select method
             }
             else{
-                perror("connect sproxy failed\n");
+                perror("connect sproxy failed, attemping to reconnect\n");
             }
             close(ns);
             close(s);
@@ -155,7 +232,9 @@ int main (int argc, char * argv[])
             perror("telnet fail");
         }
     }
+    return 1;
 }
+
 
 
 
--- server.c
+++ server.c
@@ -9,14 +9,46 @@
 #include <netdb.h>
 #include <string.h>
 #include <unistd.h>  // Include for close function
+#include <arpa/inet.h>// Include for inet_addr function
+#include <time.h>
+struct Header{
+    int messageType;// 1 = actual data, 0 = heartbeat message
+    int ack; // acknowledgment number
+    int seq; // sequence number
+    int sessionID;// keep track of session
+};
+void setAndSendHeader(int socket, int seq, int ack, int sID, int msg_type){
+    struct Header header;
+    header.sessionID = sID;
+    header.messageType = msg_type;
+    header.seq = seq;
+    header.ack = ack;
+    char headerBuf[sizeof(struct Header)];
+    memcpy(headerBuf, &header, sizeof(struct Header));
+    send(socket, headerBuf, sizeof(struct Header), 0);//send the header
+}
 
-void callSelect(int td, int ns) {//select() code
-
+void sendHeartBeat(int s, int sID){
+    struct Header header;
+    header.sessionID = sID;
+    header.messageType = 0;
+    header.ack = -1;
+    header.seq = -1;
+    char headerBuf[sizeof(struct Header)];
+    memcpy(headerBuf, &header, sizeof(struct Header));
+    send(s, headerBuf, sizeof(struct Header), 0);//send the first heartbeat message to sproxy
+    uint32_t networkPayload = htonl((uint32_t)sID);
+    send(s, &networkPayload, sizeof(uint32_t), 0);// send the heartbeat playload to sproxy
+}
+void callSelect(int td, int ns, int sID) {//select() code
+    int seq_C, ack_C, seq_S, ack_S = -1;
+    int sessionID = sID;
+    time_t lastReceiveTime, lastTransmitTime;
     while(1){
         int n, MAX_LEN = 256;
         struct timeval tv;
         fd_set readfds;
-        char buf1[MAX_LEN], buf2[MAX_LEN];
+//        char buf1[MAX_LEN], buf2[MAX_LEN];
         //clear the set
         FD_ZERO(&readfds);
         //add descriptors to the set
@@ -29,8 +61,8 @@ void callSelect(int td, int ns) {//select() code
             n = ns + 1;
 
         //wait until either socket has data ready to recv() timeout 10.5 secs
-        tv.tv_sec = 10;
-        tv.tv_usec = 500000;
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
         //select will return when at least one of the sockets has incoming traffic, or after timeout
         int rv = select(n, &readfds, NULL, NULL, &tv);
 
@@ -40,26 +72,69 @@ void callSelect(int td, int ns) {//select() code
             return;
         }
         else if (rv == 0) {
-            printf("Timeout occurred! No data after 10.5 seconds\n");
+            //printf("Timeout occurred! No data after 1 seconds\n");
+            time_t newSendTime, newReceiveTime;
+            newSendTime = time(NULL);
+            newReceiveTime = time(NULL);
+            if ((newSendTime - lastTransmitTime) >= 1){
+                //todo send a heart beat message
+                sendHeartBeat(ns, sessionID);
+            }
+            else if((newReceiveTime - lastReceiveTime) >= 3){
+                //todo timeout need to reconnect
+                return;//since this connect is already lost
+            }
         }
         else{
             // one or both of the descriptors have data
             if (FD_ISSET(td, &readfds)) {
+                char buf1[MAX_LEN];
                 ssize_t fromTDaemon_len = recv(td, buf1, MAX_LEN, 0);//receive data from telnet daemon
-                //buf1[fromTDaemon_len] = '\0';
-                //printf("%s\n", buf1);//print received data
-                if (fromTDaemon_len == 0)
-                    return;
-                send(ns, buf1, fromTDaemon_len, 0);//send data to cproxy
+                if (fromTDaemon_len > 0){
+                    if (seq_S == -1){// if the first data transmit, initialize seq, ack
+                        seq_S = 0;
+                        ack_S = 0;
+                    }
+                    //TODO set and send header
+                    setAndSendHeader(ns, seq_S, ack_S, sessionID, 1);//send header to cproxy
+                    send(ns, buf1, fromTDaemon_len, 0);//send data to cproxy
+                    lastTransmitTime = time(NULL);//update lastTransmitTime once send out data to cproxy
+                }
+                else if (fromTDaemon_len == 0)
+                    return;//connection closed, return
+                else
+                    return;//error happened, return
             }
 
-            if (FD_ISSET(ns, &readfds)) {
+            if (FD_ISSET(ns, &readfds)) {// if receive data from cproxy
+                char buf2[MAX_LEN];
+                char headerBuf[sizeof(struct Header)];
+                if (recv(ns, headerBuf, sizeof(struct Header), 0) < 0){//if receives fail
+                    perror("error receiving header");
+                }
+                struct Header *receivedHeader = (struct Header *)headerBuf;//make the header buffer into Header struct
+                printf("Received Header - messageType: %d, Seq: %d, Ack: %d, sessionID: %d\n", receivedHeader->messageType, receivedHeader->seq, receivedHeader->ack, receivedHeader->sessionID);
+                seq_C = receivedHeader->seq;
+                ack_C = receivedHeader->ack;
+                seq_C++;
+                ack_C++;
                 ssize_t fromCproxy_len = recv(ns, buf2, MAX_LEN, 0);//receive data from cproxy
-                if (fromCproxy_len == 0)
-                    return;
+                lastReceiveTime = time(NULL);//update last receiveTime once receive data from cproxy
+                if (fromCproxy_len > 0){
+                    if (receivedHeader->messageType == 0){
+                        printf("it's a heart beat message\n");
+                    }
+                    else{
+                        send(td, buf2, fromCproxy_len, 0);//send data to telnet daemon
+                    }
+                }
+                else if (fromCproxy_len == 0)
+                    return;//connection closed, return
+                else
+                    return;//error happened, return
                 //buf2[fromCproxy_len] = '\0';
                 //printf("%s\n", buf2);//print received data
-                send(td, buf2, fromCproxy_len, 0);//send data to telnet daemon
+
             }
         }
     }
@@ -78,41 +153,47 @@ int main(int argc, char * argv[]) {
 
     // bind server address struct to server socket
     int bind_status = bind(s, (struct sockaddr *) &myAddr, sizeof(myAddr));
-    //printf("sproxy bind_status%d\n", bind_status);
     if (bind_status < 0) {
-        perror("to daemon socket bind");
+        perror("to cproxy socket bind");
         exit(EXIT_FAILURE);
     }
-
     // keep at most 5 connections
-    listen(s, 5);
-
+    int listen_status = listen(s, 5);
+    if (listen_status < 0){
+        perror("to cproxy socket listen");
+        exit(EXIT_FAILURE);
+    }
     // client address/port
     struct sockaddr_in cliAddr;
     bzero(&cliAddr, sizeof(cliAddr));
     socklen_t len = sizeof(cliAddr);
-//    // new socket for new connections
-//    ns = accept(s, (struct sockaddr *) &cliAddr, &len);
 
-    char buf[1024];
-    // constantly receive client address/port
-//    while (1) {
-//        // new socket for new connections
-//        int ns = accept(s, (struct sockaddr *) &cliAddr, &len);
-//        uint32_t n;//the 4 byte length of playload
-//        while (recv(ns, &n, 4, 0) > 0) {// keep receive message from client, receive the 4 byte int, length of playload
-//            recv(ns, buf, ntohl(n), 0);//
-//            buf[ntohl(n)] = '\0';  // Add null terminator to properly terminate the buffer
-//            printf("%-4u\t%s", ntohl(n), buf);
-//            memset(buf, 0, sizeof(buf));  // Reset buf
-//        }
-//
-//        close(ns);
-//    }
     while(1) {
+        int sessionID = -1;// keep track of this session
+        printf("accepting sockets\n");
         int ns = accept(s, (struct sockaddr *) &cliAddr, &len);
         if (ns > 0) {
-            //printf("connect with cproxy success\n");
+            printf("connect with cproxy success\n");
+
+            //receive first heartBeat message from cproxy
+            char headerBuf[sizeof(struct Header)];
+            if (recv(ns, headerBuf, sizeof(struct Header), 0) < 0){//if receives fail
+                perror("error receiving header");
+            }
+            struct Header *receivedHeader = (struct Header *)headerBuf;//make the header buffer into Header struct
+            //printf("Received Header - messageType: %d, Seq: %d, Ack: %d, sessionID: %d", receivedHeader->messageType, receivedHeader->seq, receivedHeader->ack, receivedHeader->sessionID);
+            if (sessionID == -1){
+                sessionID = receivedHeader->sessionID;
+            }
+            char buf1[256];
+            ssize_t fromCproxy_len = recv(ns, buf1, 256, 0);//receive first data from cproxy
+            if (fromCproxy_len > 0)
+                printf("recieve first heartbeat success\n");
+            else if (fromCproxy_len == 0)
+                return 0;//connection closed, return
+            else
+                return 0;//error happened, return
+
             // td socket code
             int td = socket(PF_INET, SOCK_STREAM, 0);//for connection to telnet daemon
             //telnet daemon struct
@@ -126,19 +207,12 @@ int main(int argc, char * argv[]) {
 
             // connect sproxy client socket to telnet daemon socket
             int connection_status = connect(td, (struct sockaddr *) &telnetAddr, sizeof(telnetAddr));
-            char *line = NULL; // a null line pointer that will be pointing to the line that is being read into the buffer
-            size_t len = 0; // capacity of the buffer, initialized as 0, let getline know the initial size of buffer, and can be updated by getline to have the actual size of buffer
-            ssize_t size; // actual length of input line in bytes
+            //char *line = NULL; // a null line pointer that will be pointing to the line that is being read into the buffer
+            //size_t len = 0; // capacity of the buffer, initialized as 0, let getline know the initial size of buffer, and can be updated by getline to have the actual size of buffer
+            //ssize_t size; // actual length of input line in bytes
             if (connection_status == 0) {// if connect success
-                //printf("Connect to telnet daemon success\n");
-                callSelect(td, ns);//call the select method
-//            while ((size = getline(&line, &len, stdin)) != -1) {
-//                uint32_t networkOrderSize = htonl((uint32_t)size);  // Convert size to network byte order
-//                send(s, &networkOrderSize, 4, 0);  // Send out the size as a 4-byte integer
-//                send(s, line, size, 0);//send out the line itself
-//                //printf("%s", line);
-//            }
-//            free(line);
+                printf("Connect to telnet daemon success\n");
+                callSelect(td, ns, sessionID);//call the select method
             }
             else{
                 perror("connection to telnet daemon failed\n");
@@ -147,7 +221,10 @@ int main(int argc, char * argv[]) {
             close(ns);
             close(td);
         }
+        else{
+            printf("connect to cproxy failed\n");
+        }
     }
 
-
+    return 1;
 }
